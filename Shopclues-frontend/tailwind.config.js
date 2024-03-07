26846584e5b15082be2d33ab8c9ce505c5ab3317
Vidur Tuli <vidurtuli53@gmail.com>
2024-03-07 22:21:30 +0530

    UI started

--- Shopclues-frontend/tailwind.config.js
+++ Shopclues-frontend/tailwind.config.js
@@ -6,6 +6,28 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+        // Primary
+        white: '#ffffff',
+        black: '#000000',
+        dark: '#363738',
+
+        // Secondary
+        secondaryOne: '#F5F5F5',
+        secondaryTwo: '#FEFAF1',
+
+        // Text
+        textOne: '#FAFAFA',
+        textTwo: '#7D8184',
+
+        // Button
+        btnPrimary: '#DB4444',
+        btnPrimaryHover: '#E07575',
+        btnSecondaryHover: '#A0BCE0',
+        
+        // Colors
+        success: '#47B486',
+    }
   },
   plugins: [],
 }
